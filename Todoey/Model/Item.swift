884fd8df051e03cad6884d599e0e3c44965cd8b4
Jan van Rensburg <jan@labs.epiuse.com>
2018-09-15 15:19:35 -0400

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jan van Rensburg on 9/15/18.
+//  Copyright © 2018 Jan van Rensburg. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
